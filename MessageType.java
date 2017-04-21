e38e0bc7f621004c7eb69c0d83f0eb07f3648677
HasanKarilov <hkarilov@gmail.com>
2017-04-21 22:06:18 +0600

    Oсновные моменты протокола 03

--- MessageType.java
+++ MessageType.java
@@ -5,4 +5,10 @@ package com.javarush.task.task30.task3008;
  * Created by hanaria on 4/21/17.
  */
 public enum MessageType {
+    NAME_REQUEST, // – запрос имени.
+    USER_NAME, // – имя пользователя.
+    NAME_ACCEPTED, // – имя принято.
+    TEXT, // – текстовое сообщение.
+    USER_ADDED, // – пользователь добавлен.
+    USER_REMOVED; // – пользователь удален.
 }
